9d122c7f4040f8efcedfb194418521019bb42d56
Marceau <mamar123095@gmail.com>
2026-02-14 22:38:21 +0100

    icone

--- pages/header.php
+++ pages/header.php
@@ -12,6 +12,7 @@ $userPrenom = valider("prenom", "SESSION");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CrokNotes</title>
+    <link rel="icon" type="image/png" href="ressources/icones/couverts.png">
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <script src="https://unpkg.com/lucide@latest"></script>
